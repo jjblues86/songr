afb1463b6c77b58068a856edee5f5c77820ff38f
Jerome Joof <jeromejoof@yahoo.com>
2020-01-22 12:01:05 -0800

    all features completed

---
+++ src/main/java/com/jjblues86/songr/AlbumRepository.java
@@ -0,0 +1,10 @@
+package com.jjblues86.songr;
+
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+//    void getOne(Album updateAlbum);
+//    void getOne(Album updateAlbum);
+}
+
--- src/main/java/com/jjblues86/songr/HomeController.java
+++ src/main/java/com/jjblues86/songr/HomeController.java
@@ -1,13 +1,23 @@
 package com.jjblues86.songr;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    //Instance variable to hold repository
+    @Autowired
+    AlbumRepository albumRepository;
 
     @GetMapping("/")
     public String getHome(){
@@ -29,11 +39,26 @@ public class HomeController {
 
     @GetMapping("/albums")
     public String getAlbums(Model m){
-        Album[] albums = new Album[] {new Album("Dreams and NightMares", "Meek Mill", 20, 240, "www.meekmill.com")};
+        List<Album> albums = albumRepository.findAll();
+//        Album[] albums = new Album[] {new Album("Dreams and NightMares", "Meek Mill", 20, 240, "www.meekmill.com")};
 //        Albums[] albumsTwo = new Albums[] {new Albums("I am the best", "Jabin Joof", 5, 100, "www.jabin.com")};
 //        Albums[] albumsThree = new Albums[] {new Albums("Rescue", "Jay Ottom", 10, 440, "www.jay.com")};
 
         m.addAttribute("rapAlbums", albums);
         return "albums";
     }
+    @PostMapping("/albums")
+    public RedirectView postAllAlbums(String title, String artist, int songCount, int length, String imageUrl, Model j){
+        Album newAlbum = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(newAlbum);
+//        return "albums";
+        return new RedirectView("/albums");
+    }
+//    @PutMapping("/{albumsId}")
+//    public  Album updateAlbums(@PathVariable String albumsId, String title, String artist, int songCount, int length, String imageUrl, Model u){
+//        Album updateAlbum = new Album(title, artist, songCount, length, imageUrl);
+//        albumRepository.getOne(updateAlbum.id);
+//        return updateAlbum;
+//    }
+
 }
